80afd054fb1b56c32de2f5ec63ac84c4653fcd22
Josh Miller <joshmiller1702@hotmail.com>
2025-01-07 22:37:33 +0000

    add hypergeo_calc function

--- src/__main__.py
+++ src/__main__.py
@@ -7,19 +7,32 @@ import math
 #Accept user imput in args rather than manually changing variables
 #Frontend app/ui/interface, simple but useful
 
+def hypergeo_calculator(total_desired_cards = int, number_in_hand = int, cards_drawn = 7, deck_size = 99):
+    """ 
+    This calcuclates the percentage chance that a specific type of card will be drawn from a deck of a given size.
+
+    Args:
+        total_desired_cards: The amount of a desired card type in the deck
+        cards_drawn: The total number of cards drawn
+        number_in_hand: The number of a desired card type wanted in hand
+        deck_size: The total number of cards in deck
+    """
+    success_in_pop = ((math.comb(total_desired_cards, number_in_hand))*(math.comb(deck_size - total_desired_cards, cards_drawn - number_in_hand))) / (math.comb(deck_size, cards_drawn))
+    percentage_chance = str(round(success_in_pop*100, 2)) + "%"
+    return percentage_chance
 
 if __name__ == "__main__" :
     
     start = sys.argv[1]
+    """
+    Variables
+    """
+    deck_size = 100
+    total_desired_cards = 14
+    cards_drawn = 7
+    number_in_hand = 2
 
-    deck_size, N = 100, 100
-    desired_card_count, K = 14, 14
-    cards_drawn, n = 7, 7
-    number_in_hand, k = 2, 2
-
-    success_in_pop = ((math.comb(desired_card_count, k))*(math.comb(deck_size - desired_card_count, cards_drawn - number_in_hand))) / (math.comb(deck_size, cards_drawn))
-    percentage = round(success_in_pop*100, 2)
-    print(str(percentage) + "%")
+    
 
 
     
