3b081e5a0f10ef46c8d7104fcf3eae14366a37f2
Josh Miller <joshmiller1702@hotmail.com>
2025-01-09 22:19:54 +0000

    added deck grabber from clipboard for MTGA format

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,7 @@
 import sys
 import math
+import os
+import win32clipboard
 
 #Potential expansions listed below
 #Deck speed variable to help with below thresholds
@@ -22,7 +24,23 @@ def hypergeo_calculator(total_desired_cards = int, number_in_hand = int, cards_d
         percentage_chance = round(success_in_pop*100, 2)
         return percentage_chance
     except ValueError:
-        print("Your numbers are not compatible!")
+        print("Your numbers are not compatible, please re-enter!")
+
+def grab_deck_list():
+    """
+    This function will grab the deck list from the users clipboard and return the deck list as an array containing the card quantity and name, also returns the deck name.
+    """
+    ###!!!!!!Needs to be widened to support other deck import formats, currently only supports MTGA decklists!!!!!!###
+    win32clipboard.OpenClipboard()
+    deck_list = win32clipboard.GetClipboardData()
+    deck_list_arr = deck_list.split("\n")
+    deck_list_formatted = []
+    deck_name = deck_list_arr[1]
+    deck_name_formatted = deck_name.replace("Name ", "")
+    for i in deck_list_arr[4:]:
+        deck_list_formatted.append(i.replace("\r", ""))
+    win32clipboard.CloseClipboard()
+    return deck_list_formatted, deck_name_formatted
         
 
 if __name__ == "__main__" :
@@ -34,4 +52,6 @@ if __name__ == "__main__" :
 
     percentage_chance = hypergeo_calculator(total_desired_cards, number_in_hand, cards_drawn, deck_size)
     print(f"You will hit this draw {percentage_chance:00.2f}% of the time!")
+
+    deck_list, deck_name = grab_deck_list()
     
